7cccea94647ec8c4cfaf9836b3cd3083f4f11887
JoseSabeckis <josesabeckis_pj@hotmail.com.ar>
2023-04-29 17:48:08 -0300

    arreglos css
    buttons

--- ProyectoWebCV/src/app/components/acerca-de/acerca-de.component.ts
+++ ProyectoWebCV/src/app/components/acerca-de/acerca-de.component.ts
@@ -3,6 +3,7 @@ import { acerca } from 'src/app/models/acerca.model';
 import { persona } from 'src/app/models/persona.model';
 import { AcercaService } from 'src/app/service/acerca.service';
 import { PersonaServicioService } from 'src/app/service/persona/persona-servicio.service';
+import { TokenService } from 'src/app/service/token/token.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -13,13 +14,22 @@ export class AcercaDeComponent implements OnInit {
   persona: persona = new persona("","","","");
   acerca: acerca = new acerca("");
 
-  constructor(public personaService: PersonaServicioService, public acercaService: AcercaService){
+  isLogged = false;
+
+  constructor(public personaService: PersonaServicioService, public acercaService: AcercaService, private tokenServicio: TokenService){
     
   }
 
   ngOnInit(): void{
     this.personaService.getPersona().subscribe(data => {this.persona = data});
     this.acercaService.getAcercaDe().subscribe(data => {this.acerca = data});
+
+    if(this.tokenServicio.getToken()){
+      this.isLogged = true;
+    }
+    else{
+      this.isLogged = false;
+    }
   }
 
 }
