795d86d1d20852c79e6e2eec9e716accbe866085
JoseSabeckis <josesabeckis_pj@hotmail.com.ar>
2023-05-01 21:19:37 -0300

    F

--- ProyectoWebCV/src/environments/environment.prod.ts
+++ ProyectoWebCV/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
-    production: false,//prueba-true
-    Url: 'ultimobackcv-production.up.railway.app/',
+    firebase: {
+        projectId: 'curriculumwebfloresjose',
+        appId: '1:413804569590:web:3fac5b5ea1a6cd37b61dd7',
+        storageBucket: 'curriculumwebfloresjose.appspot.com',
+        locationId: 'us-central',
+        apiKey: 'AIzaSyCUhb1zmVYVkWhVL5SuzQTr1pnrgmUVzpE',
+        authDomain: 'curriculumwebfloresjose.firebaseapp.com',
+        messagingSenderId: '413804569590',
+        production: true,
+        Url: 'ultimobackcv-production.up.railway.app/',
+      }
 };
